f183029c46796fab16b0e2323895f63027e5550e
gferreiraas <161588787+gferreiraas@users.noreply.github.com>
2024-03-27 08:43:14 -0300

    Add files via upload

---
+++ sketch2 (1).js
@@ -0,0 +1,7 @@
+var primeiro_numero=prompt("digite primeiro numero");
+var segundo_numero=prompt("digite o segundo numero");
+var terceiro_numero=prompt("digite o terceiro numero")
+var quarto_numero=prompt("digite o quarto numero")
+var quinto_numero=prompt("digite o quinto numero")
+var resultado=Number(primeiro_numero)+Number(segundo_numero)-Number(terceiro_numero)*Number(quarto_numero)/Number(quinto_numero)
+console.log("a soma é"+ resultado)
